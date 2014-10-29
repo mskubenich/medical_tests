4a432a76fb7ce4b97fd48f178531f4cc77f52322
Misha <misha.skubenich@gmail.com>
2014-10-29 23:24:59 +0200

    Pagination for questions.

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -7,7 +7,7 @@ class QuestionsController < ApplicationController
   # GET /questions
   # GET /questions.json
   def index
-    @questions = @profile.questions
+    @questions = @profile.questions.paginate(:page => params[:page], per_page: 10).order('created_at DESC')
   end
 
   # GET /questions/1
